3192ef3da574facb6af950c7fec6aa9a4aed3b69
Christian Hollinger <christian@chollinger.com>
2024-02-17 11:40:39 -0500

    feat: NoWorkersAvailableException

--- modules/shared/src/main/scala/com/chollinger/bridgefour/shared/exceptions/Exceptions.scala
+++ modules/shared/src/main/scala/com/chollinger/bridgefour/shared/exceptions/Exceptions.scala
@@ -2,10 +2,11 @@ package com.chollinger.bridgefour.shared.exceptions
 
 object Exceptions {
 
-  class BridgeFourException(msg: String = "")          extends Exception(msg)
-  case class InvalidWorkerConfigException(msg: String) extends BridgeFourException(msg)
-  case class OrphanTaskException(msg: String) extends BridgeFourException(msg)
-  case class NoWorkersAvailableException(msg: String)  extends BridgeFourException(msg)
-  case class NoFilesAvailableException(msg: String)    extends BridgeFourException(msg)
+  class BridgeFourException(msg: String = "")           extends Exception(msg)
+  case class InvalidWorkerConfigException(msg: String)  extends BridgeFourException(msg)
+  case class OrphanTaskException(msg: String)           extends BridgeFourException(msg)
+  case class NoWorkersAvailableException(msg: String)   extends BridgeFourException(msg)
+  case class NoFilesAvailableException(msg: String)     extends BridgeFourException(msg)
+  case class MisconfiguredClusterException(msg: String) extends BridgeFourException(msg)
 
 }
