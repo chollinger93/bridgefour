ddd06808e201651bbc0e9bbc38d77993abfc3a0b
Christian Hollinger <christian@chollinger.com>
2024-02-19 10:40:37 -0500

    feat: More functions on Persistence

--- modules/shared/src/main/scala/com/chollinger/bridgefour/shared/persistence/Persistence.scala
+++ modules/shared/src/main/scala/com/chollinger/bridgefour/shared/persistence/Persistence.scala
@@ -29,68 +29,53 @@ import cats.implicits.toTraverseOps
 import cats.syntax.all.toTraverseOps
 import cats.syntax.traverse.toTraverseOps
 import com.chollinger.bridgefour.shared.types.Typeclasses.ThrowableMonadError
+
 sealed trait Persistence[F[_], K, V] {
 
-  def put(key: K, value: V): F[Option[V]]
+  def put(key: K, value: V): F[Unit]
+
+  def update(identity: V)(key: K, v: V => V): F[Unit]
 
   def get(key: K): F[Option[V]]
 
   def del(key: K): F[Option[V]]
 
-}
+  def keys(): F[List[K]]
 
-sealed trait Counter[F[_], K] {
+  def values(): F[List[V]]
 
-  def inc(key: K): F[Unit]
+  def list(): F[Map[K, V]]
 
-  def dec(key: K): F[Unit]
-  
-  def set(key: K, value: Long): F[Unit]
-  
-  def get(key: K): F[Long]
 }
 
 object InMemoryPersistence {
 
   def makeF[F[_]: Sync, K, V](): F[Persistence[F, K, V]] = {
+    val data = new ConcurrentHashMap[K, V]()
     for {
-      storage <- MapRef.ofScalaConcurrentTrieMap[F, K, V]
-    } yield new Persistence[F, K, V]() {
+      storage <- Ref.of[F, Map[K, V]](data.asScala.toMap)
+    } yield {
+      new Persistence[F, K, V]() {
 
-      def put(key: K, value: V): F[Option[V]] = storage(key).getAndSet(Some(value))
+        def put(key: K, value: V): F[Unit] = storage.getAndUpdate(_.updated(key, value)).void
 
-      def get(key: K): F[Option[V]] = storage(key).get
+        def update(identity: V)(key: K, v: V => V): F[Unit] = storage
+          .getAndUpdate(m => m.updated(key, v(m.getOrElse(key, identity))))
+          .void
 
-      def del(key: K): F[Option[V]] = storage(key).getAndSet(None)
-    }
-  }
+        def get(key: K): F[Option[V]] = storage.get.map(_.get(key))
 
-}
+        def del(key: K): F[Option[V]] = for {
+          old <- storage.get.map(_.get(key))
+          _   <- storage.update(_.removed(key))
+        } yield old
 
-object InMemoryCounter {
+        def keys(): F[List[K]] = storage.get.map(_.keys.toList)
 
-  private type MR[F[_]] = MapRef[F, Int, Option[Long]]
+        def values(): F[List[V]] = storage.get.map(_.values.toList)
 
-  private def init[F[_]: Applicative](s: MR[F], size: Int): F[Unit] = for {
-    _ <- Range.inclusive(0, size).toList.traverse(i => s(i).set(Some(0L)))
-  } yield ()
-  def makeF[F[_]: Async: Applicative: ThrowableMonadError](size: Int = 10000) = {
-    for {
-      storage <- MapRef.ofScalaConcurrentTrieMap[F, Int, Long]
-      _       <- init(storage, size)
-    } yield new Counter[F, Int]() {
-      val err: ThrowableMonadError[F] = implicitly[ThrowableMonadError[F]]
-
-      def inc(key: Int): F[Unit] = storage.updateKeyValueIfSet(key, v => v + 1L)
-
-      def dec(key: Int): F[Unit] = storage.updateKeyValueIfSet(key, v => v - 1L)
-
-      def set(key: Int, value: Long): F[Unit] = storage.updateKeyValueIfSet(key, _ => value)
-
-      def get(key: Int): F[Long] = for {
-        r <- storage(key).get
-        _ <- err.raiseWhen(r.isEmpty)(new Exception(s"Key $key exceeds counter size $size"))
-      } yield r.get
+        def list(): F[Map[K, V]] = storage.get
+      }
     }
   }
 
